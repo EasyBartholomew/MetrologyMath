a8f75c296b6584dd7802e2d509f6beb41e51b138
EasyBartholomew <easybartholomew@gmail.com>
2019-10-09 12:13:39 +0300

    Output error fixes. Numbering added.

--- MetrologyMath/main.c
+++ MetrologyMath/main.c
@@ -14,29 +14,29 @@
 #define DEFAULT_INPUT_FILE "in.txt"
 
 
-void out(cvector input) {
-	printf("Расчёты:\nN = %u\n", input.size);
+void out(cvector input, size_t num) {
+	printf("\nРасчёты #%u:\nN = %u\n", num, input.size);
 
 	int K_val = calcK(&input);
 
 	printf("K = %d;\n", K_val);
 	printf("dX = %d;\n", calcdX(&input, K_val));
-	printf("<X> = %lf;\n", calcAvgX(&input));
+	printf("<X> = %.3lf;\n", calcAvgX(&input));
 
 	double X_val = calcX(&input);
 
-	printf("X = %lf;\n", X_val);
-	printf("S = %lf;\n", calcSx(&input, X_val));
+	printf("X = %.3lf;\n", X_val);
+	printf("S = %.3lf;\n", calcSx(&input, X_val));
 
 	double* p_enum = (double*)input.stock;
 
 	printf("\nОтсортированная последовательность:\n{");
 
 	for (register unsigned i = 0; i < input.size - 1; i++) {
-		printf(" %.2lf,", p_enum[i]);
+		printf(" %.3lf,", p_enum[i]);
 	}
 
-	printf(" %.2lf }", p_enum[input.size - 1]);
+	printf(" %.3lf }", p_enum[input.size - 1]);
 }
 
 
@@ -50,22 +50,30 @@ int main() {
 	text = FReadAllToVec(DEFAULT_INPUT_FILE);
 	input = CreateCVector(sizeof(double), 0);
 
-	int last = 0;
+	int last, cnt;
 
-	printf("Содержимое файла \'%s\':\n%s\n***************************\n\n",
+	last = cnt = 0;
+
+	printf("Содержимое файла \'%s\':\n%s\n************************************\n\n",
 		DEFAULT_INPUT_FILE, (cstr_t)text.stock);
 
 	while (1) {
 
 		last = ParseDoubleEnumToCVec(&input, (cstr_t)text.stock + last, "{", "}", ",", ENUM_PARSE_SYMBOL_TYPE_ALL);
 
-		if (!last) {
+		if (!(last || cnt)) {
 			printf("Local error code: 0x%.8X;\nERRNO = 0x%.8X;\n%s", GetLastLocalERROR(), errno, strerror(errno));
 			_getch();
 			return -1;
 		}
+		else if (!last) {
+			break;
+		}
 
-		out(input);
+		cnt++;
+
+		out(input, cnt);
+
 		CVectorClear(&input);
 
 		if (last == -1)
